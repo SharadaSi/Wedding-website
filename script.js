f09f72fc21ba49e0745c9c3901713c8346a6b48a
Šárka Šimová <94860676+SharadaSi@users.noreply.github.com>
2023-06-09 17:48:09 +0200

    Add files via upload
    9.6.2023 Version 2 after code review

--- script.js
+++ script.js
@@ -1,99 +1,151 @@
+//Hamburger menu
+
+let isMenuOpen = false //výchozí stav menu
 const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu ") 
-const body = document.querySelector("body")
+const navMenu = document.querySelector(".nav-menu")
+const menuBtn = document.querySelector(".menu-btn")
+
+
+ 
+function toggleMenu() {
+    isMenuOpen = true
+    isMenuOpen = !isMenuOpen
+}
 
 // Funkce která na kliknutí jinde než na hamburger nebo dropdown menu schová dropdown menu
 const handleClickOutside = (event) => {
-    if (!navMenu.contains(event.target) && event.target !== hamburger) {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
-    }
+    if (!navMenu.contains(event.target) && event.target !== menuBtn) {
+      navMenu.classList.remove("active")
+      hamburger.classList.remove("active")
+      toggleMenu()
+    } else {}
 }
 
-hamburger.addEventListener("click", () => {
-    
-    //Na elementu .hamburger vyber classu .active a udělej s ní toggle
-    hamburger.classList.toggle("active")
+menuBtn.addEventListener("click", () => {
+    //Výchozí hodnota - menu není otevrene se na kliknutí změní na opačnou boolean hodnotu
+    isMenuOpen = !isMenuOpen
+    //V tuto chvíli je isMenuOpen true a provede se podmínka
+    if(isMenuOpen){
+        menuBtn.classList.add("open")
+        navMenu.classList.add("open")
+        
+        document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => { 
+            navMenu.classList.remove("open")
+            //Zpozdeni druhe akce, aby se stihla provest pred removnutim classy open
+            setTimeout(() => {
+                menuBtn.classList.remove("open");
+                }, 10)
 
-    //Na elementu .nav-menu vyber classu .active a udělej s ní toggle (aplikuje se na položky v .nav-menu)
-    navMenu.classList.toggle("active")
+            menuBtn.classList.remove("open")
+            toggleMenu()
+        }))
 
-    //Po kliknutí na odkaz v menu se skryje celý dropdown menu
-    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
-    }))
+        //Nefunguje :( Ví někdo proč? :D =>
+        // const body = document.querySelector("body")
+        // body.addEventListener("click", handleClickOutside)
 
-    //Po kliknutí jinde než na .hamburger nebo .nav-menu zavři dropdown menu
-    body.addEventListener("click", handleClickOutside);
-}) 
-
+    } else{
+        navMenu.classList.remove("open")
+        menuBtn.classList.remove("open")
+    }
+})
 
 
 
 //Program
 
-// const btnShowDetails = document.querySelector(".arrival .btn-show")
 
-// const btnHideDetails = document.querySelector(".btn-hide")
-// const details = document.querySelector(".details")
+const accordions = document.querySelectorAll(".accordion")
 
-
-// btnShowDetails.addEventListener("click", () => {
-//     btnShowDetails.classList.toggle("active")
-//     details.classList.toggle("active")
-// })
-// btnHideDetails.addEventListener("click", () => {
-//     details.classList.remove("active")
-//     btnShowDetails.classList.toggle("active")
-// })
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const btnShowDetails = document.querySelector(".arrival .btn-show");
-//     const btnHideDetails = document.querySelector(".btn-hide");
-//     const details = document.querySelector(".details");
+  accordions.forEach((item) => {
+      let accoHeader = item.querySelector(".accordion-header")
+      accoHeader.addEventListener("click", () => {
+          item.classList.toggle("active")
   
-//     btnShowDetails.addEventListener("click", () => {
-//       btnShowDetails.classList.toggle("active");
-//       details.classList.toggle("active");
-//     });
+          let accoContent = item.querySelector(".accordion-content")
+          if(item.classList.contains("active")){
+              accoContent.style.height = "auto"
+          } else{
+              accoContent.style.height = "0px"
+          }
+      })
   
-//     btnHideDetails.addEventListener("click", () => {
-//       details.classList.remove("active");
-//       btnShowDetails.classList.toggle("active");
-//     });
-  
-//     // Set initial state
-//     btnShowDetails.classList.remove("active");
-//     details.classList.remove("active");
-//   });
+  })
+
+
+// Ubytování 
+
+const apartmanH = document.querySelector(".apartman-heading")
+const hotelH = document.querySelector(".hotel-heading")
+const chatkyH = document.querySelector(".chatky-heading")
+const stanH = document.querySelector(".stan-heading")
+
+
+const apartmanC = document.querySelector(".apartman")
+const hotelC = document.querySelector(".hotel")
+const chatkyC = document.querySelector(".chatky")
+const stanC = document.querySelector(".stan")
+
+const removeAll = () => {
+    apartmanC.classList.remove("active")
+    hotelC.classList.remove("active")
+    chatkyC.classList.remove("active")
+    stanC.classList.remove("active")
+}
+
+// const removeUnderline = () => {
+//     apartmanH.style.textDecoration = "none"
+//     hotelH.style.textDecoration = "none"
+//     penzionH.style.textDecoration = "none"
+//     chatkyH.style.textDecoration = "none"
+//     stanH.style.textDecoration = "none"
+// }
+
+// const addUnderline = () => {
+//     apartmanH.style.textDecoration = "underline"
+//     apartmanH.style.textDecorationColor = "#282828"
+//     apartmanH.style.textUnderlineOffset = "4px"
+// }
 
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    const scheduleItems = document.querySelectorAll(".schedule-list li");
-  
-    scheduleItems.forEach((item) => {
-      const btnShowDetails = item.querySelector(".btn-show");
-      const btnHideDetails = item.querySelector(".btn-hide");
-      const details = item.querySelector(".details");
-  
-      btnShowDetails.addEventListener("click", () => {
-        btnShowDetails.classList.toggle("active");
-        details.classList.toggle("active");
-      });
-  
-      btnHideDetails.addEventListener("click", () => {
-        details.classList.remove("active");
-        btnShowDetails.classList.toggle("active");
-      });
-  
-      // Set initial state
-      btnShowDetails.classList.remove("active");
-      details.classList.remove("active");
-    });
-  });
+apartmanH.addEventListener("click", () => {
+    removeAll()
+    apartmanC.classList.add("active")
+    
+    if (!apartmanH.classList.contains("active")){
+        apartmanC.style.display = "block"
+    }
+})
 
+hotelH.addEventListener("click", () => {
+    removeAll()
+    hotelC.classList.add("active")
+    
+    if (!apartmanH.classList.contains("active")){
+        apartmanC.style.display = "none"
+    }
+})
+
+
+
+chatkyH.addEventListener("click", () => {
+    removeAll()
+    chatkyC.classList.add("active")
+    
+    if (!apartmanH.classList.contains("active")){
+        apartmanC.style.display = "none"
+    }
+})
+
+stanH.addEventListener("click", () => {
+    removeAll()
+    stanC.classList.add("active")
+
+    if (!apartmanH.classList.contains("active")){
+        apartmanC.style.display = "none"
+    }
+})
 
 
